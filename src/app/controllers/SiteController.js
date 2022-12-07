2372e1564ce582774628668c118550a6e26995c0
0ttam <vtt2838@gmail.com>
2022-12-07 20:59:23 +0700

    Read from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,9 +1,12 @@
 const Course = require("../models/Course");
+const multipleMongooseToObject = require("../../util/mongoose");
 class SiteController {
   // [GET] /
   index(req, res, next) {
     Course.find({})
-      .then((courses) => res.render("home"))
+      .then((courses) => {
+        res.render("home", { courses: multipleMongooseToObject(courses) });
+      })
       .catch((err) => next(err));
   }
   // [GET] /Search
